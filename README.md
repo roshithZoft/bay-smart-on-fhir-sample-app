ce70600d0d2541a38d42a02db3aef3aadcca30bd
roshith.k <roshith.k@experionglobal.com>
2022-08-25 17:28:24 +0530

    changed the re-direct uri and added the details in readme file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # bay-smart-on-fhir-sample-app
 sample smart on fhir provider facing app that can be launched from Cerner and EPIC EHR's.
+
+
+FHIR client at https://github.com/smart-on-fhir/client-js
+
+This codebase can be used against EPIC EHR and Cerner EHR by only changing the client id .
+
+
+
--- launch.js
+++ launch.js
@@ -4,6 +4,6 @@ function getFhirData() {
     FHIR.oauth2.authorize({
         'client_id': clientId,
         'scope': 'user/Patient.read launch openid profile',
-        'redirect_uri': 'https://abhishekskini1317.github.io/SMART-on-FHIR.github.io/app.html'
+        'redirect_uri': 'https://roshithzoft.github.io/bay-smart-on-fhir-sample-app/app.html'
     });
 }
