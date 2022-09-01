91f8893e4e87cab408d0955a023dfa63fd23bb6e
roshith.k <roshith.k@experionglobal.com>
2022-09-01 22:28:56 +0530

    removed the debugger

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ function parseJwt(token) {
   // const headerData = decodeURIComponent(window.atob(base64Header).split('').map(function(c) {
   //   return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
   // }).join(''));
- debugger;
+ 
   var base64Url = token.split('.')[1];
   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
   var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
